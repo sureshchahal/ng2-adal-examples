4a155a0d4a1a4a804f6c00867d4c874ba1cb8e5b
sureshchahal <sureshchahal@gmail.com>
2016-10-28 11:10:42 -0500

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Angular App using ng2-adal for Azure Ad authentication.</h1>'
+})
+export class AppComponent { }
